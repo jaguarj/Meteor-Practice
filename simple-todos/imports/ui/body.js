19550a47191705cce631b7f4dc08dfe2a0bdae2a
Josh Seipel <joshseipel@gmail.com>
2017-06-15 16:38:15 -0400

    Updated todos.

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -9,4 +9,20 @@ Template.body.helpers({
 	},
 });
 
+Template.body.events({
+	'submit .new-task'(event) {
+		event.preventDefault();
+
+		const target = event.target;
+		const text = target.text.value;
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		target.text.value = '';
+	},
+});
+
 
